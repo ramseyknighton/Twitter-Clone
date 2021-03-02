5c680aaeb701998bddce1ce00118d8a22f1d8caa
Ramsey Knighton <ramseyknighton@gmail.com>
2021-03-01 22:41:51 -0600

    init commit

---
+++ src/main/java/com/tts/TechTalentTwitter/model/Role.java
@@ -0,0 +1,28 @@
+package com.tts.TechTalentTwitter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long id;
+    private String role;
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", role=" + role + "]";
+		}
+}
